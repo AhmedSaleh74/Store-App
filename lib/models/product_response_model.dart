8302e80fb2896c320bce3c410961ec9d5ff23743
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-04-20 21:32:54 +0200

    feat(models): add Product and Rating models
    - Added Product model with fields: title, description, price, etc.
    - Added Rating model with fields: rate,count

---
+++ lib/models/product_response_model.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:store_app/models/rating_model.dart';
+
+ProductResponse productResponseFromJson(String str) =>
+    ProductResponse.fromJson(json.decode(str));
+
+String productResponseToJson(ProductResponse data) =>
+    json.encode(data.toJson());
+
+class ProductResponse {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final Rating rating;
+
+  ProductResponse({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductResponse.fromJson(Map<String, dynamic> json) =>
+      ProductResponse(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"]?.toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+        rating: Rating.fromJson(json["rating"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": rating.toJson(),
+      };
+}
